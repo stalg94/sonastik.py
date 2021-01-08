c84fc5a20e42931751e23838a45543b80adc9828
stalg94 <76990526+stalg94@users.noreply.github.com>
2021-01-08 13:29:08 +0200

    Update sonastik.py

--- sonastik.py
+++ sonastik.py
@@ -3,25 +3,25 @@ from mymodule import*
 rus=[]
 engl=[]
 
-rus = loe_fail("C:\Users\stalg\Desktop\sõnastik\sonastik1\rus.txt")
+rus = loe_fail(r"C:\Users\stalg\Desktop\sõnastik\sonastik1\rus.txt")
 engl=loe_fail("engl.txt")
 while True:
     v=input("1 - tõlkimine,\n2 - lisamine,\n3 - sõnavara kontroll,\n4 - sõna rääkimine \n=> ")
     if v=="1":
-        tõlkimine(rus,engl,"rus.txt","engl.txt")
+        tõlkimine(rus,engl,r"C:\Users\stalg\Desktop\sõnastik\sonastik1\rus.txt",r"C:\Users\stalg\Desktop\sõnastik\sonastik1\engl.txt")
     elif v=="2":
         text=input("введи новое слово:")
         if text[0] in u"\u039F":
-            kirjuta_fail("rus.txt",text)
+            kirjuta_fail(r"C:\Users\stalg\Desktop\sõnastik\sonastik1\rus.txt",text)
             a=input("а теперь его перевод => ")
-            kirjuta_fail("engl.txt",a)
+            kirjuta_fail(r"C:\Users\stalg\Desktop\sõnastik\sonastik1\engl.txt",a)
             print(text," - ",a)
             rus.append(text)
             engl.append(a)
         else:
-            kirjuta_fail("engl.txt",text)
+            kirjuta_fail(r"C:\Users\stalg\Desktop\sõnastik\sonastik1\engl.txt",text)
             a=input("а теперь его перевод => ")
-            kirjuta_fail("rus.txt",a)
+            kirjuta_fail(r"C:\Users\stalg\Desktop\sõnastik\sonastik1\rus.txt",a)
             print(text," - ",a)
             engl.append(text)
             rus.append(a)
